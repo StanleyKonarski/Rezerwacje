d6ec641105ef875ca36519bfcbd9bd6093407514
StanleyKonarski <59651390+StanleyKonarski@users.noreply.github.com>
2021-01-03 14:59:18 +0100

    koljne taski wykonane

--- app/Http/Controllers/AddReservationController.php
+++ app/Http/Controllers/AddReservationController.php
@@ -9,12 +9,27 @@ use Carbon\Carbon;
 class AddReservationController extends Controller
 {
     function addReservation(Request $req){
+        $date = Carbon::now();
+        $date->toDateTimeString();
+        $date->toDateString();
+        $from = $req->from;
+        //sprawdzenie czy poczatek rezerwacji jest w przyszłości
+        $result = $date->gt($from);
+        if($result){
+            return redirect()->back() ->withInput()->withErrors(['from_past' => 'Rezerwacja musi zaczynać się w przyszłości.']);
+        }
+        //sprawdzenie czy koniec jest po początku
+        $to = $req->to;
+        $to = Carbon::parse($to);
+        $result = $to->gt($from);
+        if(!$result){
+            return redirect()->back() ->withInput()->withErrors(['to_future' => 'Koniec rezerwacji musi następować po początku.']);
+        }
         $reservation = new Reservation;
-        $reservation->from=$req->from;
-        $reservation->to=$req->to;
+        $reservation->from=$from;
+        $reservation->to=$to;
         $reservation->user_id=2;
-        //$reservation->updated_at=Carbon::now();
-        //$reservation->created_at=Carbon::now();
         $reservation->save();
+        return redirect()->back()->with('message', 'Pomyślnie dokonano rezerwacji !');
     }
 }
--- resources/views/add_reserv.blade.php
+++ resources/views/add_reserv.blade.php
@@ -1,9 +1,24 @@
-<html>
-    <h1>Dokonaj rezerwacji</h1>
-    <form action="add" method="POST">
-        @csrf
-        <input type="date" name="from"><br>
-        <input type="date" name="to">
-        <button type="submit">Zarezerwuj</button>
-    </form>
+@include('header_logged')
+        <h1 class="display-4">Dokonaj rezerwacji</h1>
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
+        @if(session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+        @endif
+        <form action="add" method="POST">
+            @csrf
+            <input type="date" name="from" value="{{ old('from') }}"><br>
+            <input type="date" name="to" value="{{ old('to') }}">
+            <button type="submit">Zarezerwuj</button>
+        </form>
+    </body>
 </html>
